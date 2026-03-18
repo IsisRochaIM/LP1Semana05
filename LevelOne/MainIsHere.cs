fd3c64039d3265387085d0d9eda7abdf60bb8ad4
Isis <mejaikatwt@gmail.com>
2026-03-18 20:09:56 +0000

    Add class doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -2,8 +2,12 @@
 
 namespace LevelOne
 {
-    public class Program
+    public class MainIsHere
     {
+        /// <summary>
+        /// The main method is the entry point of a C# console application. When you run the application, the Main method is executed first. In this example, it simply prints "Hello LP!" to the console.
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
